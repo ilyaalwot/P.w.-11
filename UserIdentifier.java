62640728876edbdc4e118beec3139b3d7e8967f0
ilyaalwot <ghostreaper2765@gmail.com>
2026-06-22 18:45:04 +0300

    .

---
+++ UserIdentifier.java
@@ -0,0 +1,37 @@
+import java.util.Objects;
+
+public class UserIdentifier {
+    private int id;
+    private String name;
+
+    public UserIdentifier(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        UserIdentifier that = (UserIdentifier) o;
+        return id == that.id && Objects.equals(name, that.name);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name);
+    }
+
+    @Override
+    public String toString() {
+        return "UserIdentifier{id=" + id + ", name='" + name + "'}";
+    }
+}
